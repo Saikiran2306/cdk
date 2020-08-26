b545b7c6f95cbd7257dcbcccd822c29559a887fa
Perugu Sai Kiran <53146094+Saikiran2306@users.noreply.github.com>
2020-08-26 09:46:47 +0530

    Update TestShape.java

--- core_java/day2_assignment3_shape/TestShape.java
+++ core_java/day2_assignment3_shape/TestShape.java
@@ -1,22 +1,24 @@
 package day2_assignment3_shape;
 
 public class TestShape {
-	
+
 	public static void main(String[] args) {
-		
-		Calculation circle=new Circle(10);
-		System.out.println("Area of circle is "+circle.area());
-		System.out.println("Perimeter of circle is "+circle.perimeter());
+
+		Calculation shape;
+
+		shape = new Circle(10);
+		System.out.println("Area of circle is " + shape.area());
+		System.out.println("Perimeter of circle is " + shape.perimeter());
 		System.out.println();
-		
-		Calculation rectangle=new Rectangle(9.5,5);
-		System.out.println("Area of rectangle is "+rectangle.area());
-		System.out.println("Perimeter of rectangle is "+rectangle.perimeter());
+
+		shape = new Rectangle(9.5, 5);
+		System.out.println("Area of rectangle is " + shape.area());
+		System.out.println("Perimeter of rectangle is " + shape.perimeter());
 		System.out.println();
-		
-		Calculation square=new Square(5.5);
-		System.out.println("Area of square is "+square.area());
-		System.out.println("Perimeter of square is "+square.perimeter());
+
+		shape = new Square(5.5);
+		System.out.println("Area of square is " + shape.area());
+		System.out.println("Perimeter of square is " + shape.perimeter());
 	}
 
 }
